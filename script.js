3f5d4d20e85b81e905e009df70a9c2d34c152f5b
Usagi <118476704+C0deBunny@users.noreply.github.com>
2025-01-12 22:15:21 +0100

    Calculator in HTML and simple operator functions

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("Hello World!")
+let number1 = 0
+let number2 = 0
+let operator = ""
+
+function add(a, b) {
+	return a + b
+}
+
+function subtract(a, b) {
+	return a - b
+}
+
+function multiply(a, b) {
+	return a * b
+}
+
+function divide(a, b) {
+	return Math.round((a / b) * 1000000000) / 1000000000
+}
+
+function operate(a, b, c) {
+	switch (c) {
+		case "+":
+			return add(a, b)
+		case "-":
+			return subtract(a, b)
+		case "*":
+			return multiply(a, b)
+		case "/":
+			return divide(a, b)
+	}
+}
+
+console.log(operate(2, 2, "/"))
