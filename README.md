401e8a4be6b4e1ebec6b2d62ef416116cb96727a
Usagi <118476704+C0deBunny@users.noreply.github.com>
2025-01-13 18:19:27 +0100

    Programming at work

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 ### Description
 
 This project was made to practice with HTML, CSS and Javascript to make a functional calculator with styling.
+
+Color palette used: https://colorhunt.co/palette/0926351b42425c83749ec8b9
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let number1 = 0
-let number2 = 0
+let number1 = ""
+let number2 = ""
 let operator = ""
 
 function add(a, b) {
@@ -31,4 +31,71 @@ function operate(a, b, c) {
 	}
 }
 
-console.log(operate(2, 2, "/"))
+function clear () {
+	number1 = ""
+	number2 = ""
+	operator = ""
+	display.textContent = ""
+}
+
+const buttons = document.querySelector(".buttons")
+const display = document.querySelector(".text")
+
+buttons.addEventListener("click", (event) => {
+	// who clicked?
+	const element = event.target.textContent
+	if (element.length > 3) return
+
+	// numbers
+	if (operator == "") {
+		if (element.match(/[12345678900]/) != null) {
+			number1 == 0 ? number1 = element : number1 += element
+		}
+		if (element.match(/\./) != null && number1.match(/\./) == null) {
+			if (number1 == "") {
+				number1 = "0"
+				number1 += element
+			} else {
+				number1 += element
+			}
+		}
+		display.textContent = number1
+	} else {
+		if (element.match(/[12345678900]/) != null) {
+			number2 == 0 ? number2 = element : number2 += element
+		}
+		if (element.match(/\./) != null && number2.match(/\./) != null) {
+			number2 += element
+		}
+		display.textContent = number1 + operator + number2
+	}
+
+	// operators
+	if (element.match(/[-+/*]/) != null) {
+		if (number1 == "") {
+			alert("Invalid syntax")
+		} else {
+			operator = element
+			display.textContent = number1 + operator			
+		}
+	}
+
+	// clear
+	if (element == "C") {
+		clear()
+	}
+
+	// operate
+	if (element == "=") {
+		if (number1 == "" || operator == "" || number2 == "") {
+			alert("Invalid syntax")
+		} else {
+			number1 = parseFloat(number1)
+			number2 = parseFloat(number2)
+			const result = operate(number1, number2, operator)
+			clear()
+			display.textContent = result
+			number1 = result
+		}
+	}
+})
